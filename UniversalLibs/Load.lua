bc9ff2df9109525b90d69068e4d4f2201c4080f9
H3x0R <opengamertips@gmail.com>
2021-06-04 17:41:13 -0500

    Create Load.lua

---
+++ UniversalLibs/Load.lua
@@ -0,0 +1,6 @@
+local function Import(LibName)
+    return loadstring(game:HttpGet(string.format("https://raw.githubusercontent.com/LegitH3x0R/Roblox-Scripts/main/UniversalLibs/%s.lua", LibName)))()
+end
+
+getfenv().Get = Import("Get")
+getfenv().AI = Import("Pathfinding")
